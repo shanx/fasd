03bee449e4d0c0b0b551a7c60c3143619f841d44
Remco Wendt <r.wendt@ultimaker.com>
2019-06-24 10:57:51 +0200

    Add default aliases

--- conf.d/aliases.fish
+++ conf.d/aliases.fish
@@ -1,3 +1,13 @@
 # support oldschool autojump user
 alias j  z
 alias jj zz
+
+# default fasd aliases
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
